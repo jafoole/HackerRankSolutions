296733da10d1f6b1864eddeffb38cc065919006f
jafoole <smallbabba@gmail.com>
2016-09-09 15:49:32 -0400

    Second

--- src/HackerRankSols.java
+++ src/HackerRankSols.java
@@ -18,5 +18,30 @@ public class HackerRankSols {
     }
 
 
-    
+    /*
+  Insert Node at the end of a linked list
+  head pointer input could be NULL as well for empty list
+  Node is defined as
+  class Node {
+     int data;
+     Node next;
+  }
+*/
+    Node Insert(Node head, int data) {
+        if(head == null){
+            head = new Node();
+            head.data = data;
+        }
+        else {
+            Node temp = head;
+
+            while(temp.next != null){
+                temp = temp.next;
+            }
+            temp.next = new Node();
+            temp.next.data = data;
+        }
+        return head;
+    }
+
 }
