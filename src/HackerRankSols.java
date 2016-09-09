39ad4c0ab05f6b3ee01c26596c09219dcb61dcb5
jafoole <smallbabba@gmail.com>
2016-09-09 14:46:09 -0400

    first one

--- src/HackerRankSols.java
+++ src/HackerRankSols.java
@@ -8,17 +8,15 @@ public class HackerRankSols {
     // Print the integer data for each element of the linked list to stdout/console
     // (e.g.: using printf, cout, etc.). There should be one element per line.
 
-    
     void Print(Node head) {
 
         Node temp = head;
         while(temp != null) {
             System.out.println(temp.data);
             temp = temp.next;
-
         }
-
-
-
     }
+
+
+    
 }
